64ce85b012d7adb8184e9d381aa7f83a784d360b
sinano1107 <tftf161107@icloud.com>
2022-12-30 19:42:46 +0900

    タップしたところにキューブを置く

--- white reef/ContentView.swift
+++ white reef/ContentView.swift
@@ -7,32 +7,42 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @State var tapLocation: CGPoint?
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ARViewContainer(tapLocation: tapLocation)
+            .edgesIgnoringSafeArea(.all)
+            .onTapGesture(coordinateSpace: .global) { location in tapLocation = location }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    let tapLocation: CGPoint?
     
     func makeUIView(context: Context) -> ARView {
-        
         let arView = ARView(frame: .zero)
-        
-        let box = ModelEntity(mesh: .generateBox(size: 0.1), materials: [SimpleMaterial(color: .white, isMetallic: true)])
-        let anchor = AnchorEntity.init(plane: .any)
-        anchor.addChild(box)
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(anchor)
-        
+        // LiDARによるポリゴンを可視化
+        // arView.debugOptions.insert(.showSceneUnderstanding)
+        // LiDARによるポリゴンでオブジェクトを隠す
+        arView.environment.sceneUnderstanding.options.insert(.occlusion)
         return arView
-        
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+    func updateUIView(_ arView: ARView, context: Context) {
+        guard let tapLocation = tapLocation else { return }
+        // raycast
+        let results = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any)
+        guard let first = results.first else { return }
+        let anchor = AnchorEntity(raycastResult: first)
+        let box = ModelEntity(mesh: .generateBox(size: 0.1), materials: [SimpleMaterial(color: .cyan, isMetallic: true)])
+        // boxはそのままだと埋まってしまうので、半分高さを足す
+        box.setPosition([0, 0.05, 0], relativeTo: box)
+        anchor.addChild(box)
+        arView.scene.anchors.append(anchor)
+    }
 }
 
 #if DEBUG
