71baba59b855cc6d34acbae1892fadfff1ab41fc
sinano1107 <tftf161107@icloud.com>
2023-01-06 13:07:01 +0900

    [add] GeospatialView.swift

---
+++ white reef/GeospatialView.swift
@@ -0,0 +1,20 @@
+//
+//  GeospatialView.swift
+//  white reef
+//
+//  Created by 長政輝 on 2023/01/06.
+//
+
+import SwiftUI
+
+struct GeospatialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeospatialView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeospatialView()
+    }
+}
