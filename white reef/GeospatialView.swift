433373a34ecae674705b49092327b7b54fa8ec62
sinano1107 <tftf161107@icloud.com>
2023-01-06 20:02:15 +0900

    CLLocation周りを実装

--- white reef/GeospatialView.swift
+++ white reef/GeospatialView.swift
@@ -12,7 +12,7 @@ import ARKit
 struct GeospatialView: View {
     var body: some View {
         ARViewContainer()
-                .edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -54,14 +54,52 @@ private struct ARViewContainer: UIViewRepresentable {
             self.parent = parent
         }
         
+        func setUpGARSession() {
+            // TODO: setUpARSessionの実装
+            print("setUpGARSession()")
+        }
+        
+        func checkVPSAvailabilityWithCoordinate(_ coordinate: CLLocationCoordinate2D) {
+            // TODO: checkVPSAvailabilityWithCoordinateの実装（setUpGARSessionの実装後）
+            print("checkVPSAvailabilityWithCoordinate()")
+        }
+        
+        func checkLocationPermission() {
+            let locationManager = parent.locationManager
+            let authorizationStatus = locationManager.authorizationStatus
+            print(authorizationStatus.hashValue)
+            if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+                if locationManager.accuracyAuthorization != .fullAccuracy {
+                    fatalError("位置情報は完全な精度で許可されたものではありません。")
+                }
+                
+#if targetEnvironment(simulator)
+#else
+                // VPSの可用性を確認するために、デバイスの位置をリクエストします
+                locationManager.requestLocation()
+#endif
+                setUpGARSession()
+            } else if authorizationStatus == .notDetermined {
+                locationManager.requestWhenInUseAuthorization()
+            } else {
+                print("位置情報の取得が拒否または制限されている")
+            }
+        }
         
         func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-            print("didChangeAuthorization走ってるよ〜")
+            checkLocationPermission()
         }
         
-        func session(_ session: ARSession, didUpdate frame: ARFrame) {
-            print("アップデートされてるよ〜")
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            guard let location = locations.last else { return }
+            checkVPSAvailabilityWithCoordinate(location.coordinate)
         }
+        
+        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("locationの取得に失敗: \(error)")
+        }
+        
+        func session(_ session: ARSession, didUpdate frame: ARFrame) {}
     }
 }
 
