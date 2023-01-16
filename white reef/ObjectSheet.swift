c189d84802aeaa23324933158e7087d1aa2b6a9a
sinano1107 <tftf161107@icloud.com>
2023-01-16 12:54:45 +0900

    [delete] randomMaterial関数

--- white reef/ObjectSheet.swift
+++ white reef/ObjectSheet.swift
@@ -37,25 +37,6 @@ struct ObjectSheet: View {
     }
 }
 
-/// ランダムな設定のSimpleMaterialを返す
-func randomMaterial() -> SimpleMaterial {
-    let randomNumbers = Array(repeating: 0, count: 4)
-        .map { _ in CGFloat.random(in: 0...1) }
-    let color = UIColor(
-        red: randomNumbers[0],
-        green: randomNumbers[1],
-        blue: randomNumbers[2],
-        alpha: 1
-    )
-    let material = SimpleMaterial(
-        color: color,
-        roughness: MaterialScalarParameter(
-            floatLiteral: Float(randomNumbers[3])),
-        isMetallic: true
-    )
-    return material
-}
-
 struct ObjectSheet_Previews: PreviewProvider {
     static var previews: some View {
         VStack {}
