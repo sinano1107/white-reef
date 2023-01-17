ce172066a3a4215f6c81312d7f79b3b52cef128b
sinano1107 <tftf161107@icloud.com>
2023-01-17 19:37:06 +0900

    [change] UIkitのMapViewを表示

--- white reef/ContentView.swift
+++ white reef/ContentView.swift
@@ -13,40 +13,40 @@ var arView = ARView(frame: .zero)
 
 struct ContentView : View {
     private let objectData = ObjectData.sample
-    @State var region = MKCoordinateRegion()
     @State private var sheetIsPresented = false
     @State private var arIsPresented = false
     
     var body: some View {
         NavigationStack {
-            Map(coordinateRegion: $region,
-                interactionModes: .zoom,
-                showsUserLocation: true,
-                userTrackingMode: .constant(.follow))
-            .task {
-                let manager = CLLocationManager()
-                manager.requestWhenInUseAuthorization()
-            }
-            .ignoresSafeArea()
-            .navigationTitle("White Reef")
-            .navigationDestination(isPresented: $arIsPresented) {
-                PersistanceView(objectData: objectData)
-            }
-            .toolbar {
-                ToolbarItem(placement: .bottomBar) {
-                    Button(action: {
-                        sheetIsPresented.toggle()
-                    }) {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.largeTitle)
+            MapContainer()
+                .ignoresSafeArea()
+                .navigationTitle("White Reef")
+                .navigationDestination(isPresented: $arIsPresented) {
+                    PersistanceView(objectData: objectData)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: {
+                            sheetIsPresented.toggle()
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .font(.largeTitle)
+                        }
                     }
                 }
-            }
         }
         .sheet(isPresented: $sheetIsPresented) {
             ObjectSheet(arIsPresented: $arIsPresented, objectData: objectData)
         }
     }
+}
+
+struct MapContainer: UIViewRepresentable {
+    func makeUIView(context: Context) -> some MKMapView {
+        return MKMapView()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
 }
 
 struct ContentView_Previews : PreviewProvider {
